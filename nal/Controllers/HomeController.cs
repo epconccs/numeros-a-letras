a0e974f0b87c41bc473de19f05a77416aeaa347b
Andrei Torres <atorres@epcon.com.mx>
2019-01-24 16:32:24 -0600

    Se agregó controlador de inicio para redirigir home ( / ) a proyecto en GitHub

---
+++ nal/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace nal.Controllers
+{
+    [Route("/")]
+    public class HomeController : Controller
+    {
+        // Redirección a proyecto en GitHub
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Redirect("https://github.com/epconccs/nal");
+        }
+    }
+}
