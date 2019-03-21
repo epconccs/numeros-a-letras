3eb3ef1d509d97b07a395c50958c6f5e9d62344d
Roberto Orozco <issc.roberto@outlook.com>
2019-03-21 14:22:39 -0600

    Agregar prueba de respuesta HTTP para NalController

---
+++ nal.Test/NalControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using nal.Controllers;
+using Xunit;
+
+namespace nal.Test
+{
+    public class NalControllerTest
+    {
+
+        readonly NalController controller;
+
+        public NalControllerTest()
+        {
+            controller = new NalController();
+        }
+
+        [Fact]
+        public void Get_RespuestaOk()
+        {
+            var resultado = controller.Get(256);
+
+            Assert.IsType<OkObjectResult>(resultado);
+        }
+
+    }
+}
